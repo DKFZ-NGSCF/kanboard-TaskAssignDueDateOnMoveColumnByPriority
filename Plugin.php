42c41351fea61da41d82d89e1720614c48a54f40
lmfaber <l-faber@posteo.de>
2025-02-13 13:39:29 +0100

    Add plugin information

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,44 @@
 <?php
 
-namespace Kanboard\Plugin\AutomaticAction;
+namespace Kanboard\Plugin\TaskAssignDueDateOnMoveColumnByPriority;
 
 use Kanboard\Core\Plugin\Base;
-use Kanboard\Plugin\AutomaticAction\Action\TaskAssignDueDateOnMoveColumnByPriority;
+use Kanboard\Plugin\TaskAssignDueDateOnMoveColumnByPriority\Action\TaskAssignDueDateOnMoveColumnByPriority;
 
 class Plugin extends Base
 {
     public function initialize()
     {
         $this->actionManager->register(new TaskAssignDueDateOnMoveColumnByPriority($this->container));
+    }
+
+        public function getPluginName()
+    {
+        return 'TaskAssignDueDateOnMoveColumnByPriority';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Automatically assign a due date when moving to a column based on priority');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Lasse Faber';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/DKFZ-NGSCF/kanboard-TaskAssignDueDateOnMoveColumnByPriority';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.43';
     }
 }
